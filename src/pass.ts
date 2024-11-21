291fc2e9be654f986e8b3c1090423923424adb4f
Fathah Cr <abdulfathahkodag@gmail.com>
2024-11-21 12:50:10 +0400

    Fetch API

--- src/pass.ts
+++ src/pass.ts
@@ -25,7 +25,6 @@ export async function sendEntryPass(client: Client, message: WAWebJS.Message) {
       const sender = message.from;
       const regId = text.replace("PASS", "").trim();
       const data = await getRegDetails(regId);
-      console.log(data);
 
       if (!data || !data.name || !data.emirate) {
         await client.sendMessage(sender, "Registration not found");
@@ -33,14 +32,24 @@ export async function sendEntryPass(client: Client, message: WAWebJS.Message) {
         const name = data.name;
         const emirate = data.emirate;
 
-        const flaskResponse = await axios.get(
-          `https://pass.uaesahithyotsav.com/generate-pass?name=${name}&emirate=${emirate}&id=${text}`,
-          { responseType: "arraybuffer" }
+        const response = await fetch(
+          `https://pass.uaesahithyotsav.com/generate-pass?name=${name}&emirate=${emirate}&id=${text}`
         );
 
-        const imageBuffer = Buffer.from(flaskResponse.data, "binary");
-        const imagePath = path.join(__dirname, "pass.png");
+        // Check if the response is OK
+        if (!response.ok) {
+          console.error("Failed to fetch image:", response.statusText);
+          await client.sendMessage(sender, "Failed to generate pass");
+        }
 
+        // Get the image as an ArrayBuffer
+        const arrayBuffer = await response.arrayBuffer();
+
+        // Convert the ArrayBuffer to a Node.js Buffer
+        const imageBuffer = Buffer.from(arrayBuffer);
+
+        // Save the image buffer to a file
+        const imagePath = path.join(__dirname, "pass.png");
         fs.writeFileSync(imagePath, imageBuffer);
 
         const media = MessageMedia.fromFilePath(imagePath);
@@ -50,6 +59,7 @@ export async function sendEntryPass(client: Client, message: WAWebJS.Message) {
       }
     } catch (error) {
       console.log(error);
+      await client.sendMessage(message.from, "Error generating pass");
     }
   }
 }
